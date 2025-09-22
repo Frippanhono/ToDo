ff990a93766259e1467eff275f404032f3e25543
Frippanhono <167538940+Frippanhono@users.noreply.github.com>
2025-09-22 10:22:40 +0200

    test(addtask): lagt till test för skapa uppgift

---
+++ src/components/tests/AddTask.spec.ts
@@ -0,0 +1,15 @@
+import { addTask, Task } from "./Task";
+
+describe("addTask", () => {
+  it("ska skapa en ny uppgift", () => {
+    const tasks: Task[] = [];
+    const newTitle = "Köp mjölk";
+
+    const updatedTasks = addTask(tasks, newTitle);
+
+    expect(updatedTasks).toHaveLength(1); // listan växer
+    expect(updatedTasks[0].title).toBe(newTitle); // rätt titel
+    expect(updatedTasks[0].completed).toBe(false); // default status
+    expect(updatedTasks[0].id).toBeDefined(); // id sätts
+  });
+});
