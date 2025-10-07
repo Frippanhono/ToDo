96019f9f3e81ff6e57b63834bba151b420c85b1a
Andre Pinho <122530757+PinhoDev@users.noreply.github.com>
2025-10-07 12:48:23 +0200

    test: add delays to input actions in calendar and login tests for better stability

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,7 +1,12 @@
 describe("App start", () => {
   it("shows the login title", () => {
     cy.visit("/");
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(3000); // Wait to see page loading
+
     cy.get('[data-testid="login-title"]').should("be.visible");
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(2000); // Wait to observe the title
   });
 });
 
