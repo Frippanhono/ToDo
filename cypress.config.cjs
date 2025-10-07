3a7c5ae174ed30dc19cc776e2fb8d2766df5268d
Frippanhono <167538940+Frippanhono@users.noreply.github.com>
2025-10-07 10:18:59 +0200

    fix: update Cypress configuration and test scripts for correct base URL

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,23 +1,23 @@
 // cypress.config.cjs
-const { devServer } = require('@cypress/vite-dev-server')
-const { defineConfig } = require('cypress')
-const path = require('path')
+const { devServer } = require("@cypress/vite-dev-server");
+const { defineConfig } = require("cypress");
+const path = require("path");
 
 module.exports = defineConfig({
   video: false,
   e2e: {
     setupNodeEvents(on, config) {
-      on('dev-server:start', (options) => {
+      on("dev-server:start", options => {
         return devServer({
           ...options,
           viteConfig: {
-            configFile: path.resolve(__dirname, 'vite.config.ts'),
+            configFile: path.resolve(__dirname, "vite.config.ts"),
           },
-        })
-      })
+        });
+      });
 
-      return config
+      return config;
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000/ToDo/",
   },
-})
+});
